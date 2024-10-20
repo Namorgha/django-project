aa8cef4ffbfb28e0ecfb15d6eabc8e9f68a321ae
namorgha <namorgha94@gmail.com>
2024-10-20 05:45:19 +0100

    start working on real time moves

--- a_game/views.py
+++ a_game/views.py
@@ -12,7 +12,7 @@ from .forms import *
 def game_view(request, group_name='Ping-Pong'):
     game_room = get_object_or_404(GameModel, room_name=group_name)
     
-    print(f"Before adding, players in the room: {game_room.players.all()}")  # Check existing players
+    print(f"Before adding, players in the room: {game_room.players.all()}")
     if request.user not in game_room.players.all():
         if game_room.players.all().count() != 2: 
             game_room.players.add(request.user)
@@ -22,8 +22,7 @@ def game_view(request, group_name='Ping-Pong'):
     else:
         return render(request, 'a_game/gameisfull.html', {'game_room' : game_room })
     
-    print(f"After addingplayers in the room: {game_room.players.all()}")  # Check updated players
-    
+    print(f"After addingplayers in the room: {game_room.players.all()}")     
     return render(request, 'a_game/game.html', {'game_room': game_room})
 
 @login_required
@@ -33,11 +32,8 @@ def create_gameroom(request):
     if request.method == 'POST':
         form = NewGameForm(request.POST)
         if form.is_valid():
-            # Save the new game room but don't add the creator as a player yet
             new_gameroom = form.save(commit=False)
             new_gameroom.save()
-
-            # Redirect to the game view where players will be added when accessing
             return redirect('game', group_name=new_gameroom.room_name)
     
     context = {
